cf2d124a243019e00c4eeca14f68cddae51cb4bf
João Roberto <jbetol@gmail.com>
2017-03-09 22:21:36 -0300

    Third project: substrings

---
+++ substrings.rb
@@ -0,0 +1,23 @@
+def substrings(string_to_compare,dictionary)
+	string_to_compare_to_arr = string_to_compare.downcase.split(" ") # Convert string in array of downcased words
+
+	hash_result = Hash.new(0)
+	
+	dictionary.each do |word| #Gets each word of the dictionary and compares to array of words
+		string_to_compare_to_arr.each do |word_of_string_to_compare| 
+			if word_of_string_to_compare[word]			
+				hash_result[word] += 1 
+			end #if	
+		end #each
+	end #each
+
+	return hash_result
+end #def
+ 
+ dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+ 
+ puts substrings("below", dictionary)
+ puts  substrings("Howdy partner, sit down! How's it going?", dictionary)
+ 
+ 
+ # [a-zA-Z0-9./]
