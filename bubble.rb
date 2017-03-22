c080d7c9753deb66a7a9b6eabd4abdd746d5bf47
jrobertolima <jbetol@gmail.com>
2017-03-22 08:28:13 -0300

    Final bubblesort commit

--- bubble.rb
+++ bubble.rb
@@ -1,5 +1,5 @@
 def bubble_sort(array)
-	0.upto(array.length) do |iteration| #there's no need to scan all itens after first iteration
+	0.upto(array.length) do |iteration| #there's no need to scan all itens after prior iterations
 		array.each_with_index do |item, index|
 			if (index < (array.length-1 - iteration)) && (array[(index+1)] < item)
 				array[index], array[(index+1)] = array[(index+1)], item
@@ -11,10 +11,12 @@ end
 
 def bubble_sort_by(array)
 	if block_given?		
-		0.upto(array.length) do 
-			array.each_with_index do |item, index|
-				if (yield(array[(index-1)],item) > 0)
-					array[(index-1)], array[(index)] = item, array[(index-1)] 
+		arr_size = array.length - 1
+		for i in (0..arr_size) do 
+			for j in (1..(arr_size -i)) do #no need do iter over all itens after prior iterations
+				if (yield(array[(j-1)],array[j]) > 0)
+					ind_aux = array[j-1]
+					array[(j-1)], array[j] = array[j],ind_aux
 				end	
 			end
 		end
@@ -24,5 +26,5 @@ def bubble_sort_by(array)
 	end
 	return(array)
 end
-puts bubble_sort_by(["hi","hello","hey","bola","hay"]) {|left,right| left.length - right.length}
+puts bubble_sort_by(["hi","hello","567662","hey","bola","hay"]) {|left,right| left.length - right.length}
 puts bubble_sort([9,14,2,4,7,8,6])
